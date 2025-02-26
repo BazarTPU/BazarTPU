8211553e701aaba4851bdcc97a313caf456c030b
Aleksey <al.red197218@gmail.com>
2025-02-26 22:51:35 +0700

    Удаление тестовой директории.
    Удаление тестовой директории.

--- Домашнее задание к Уроку №1/task1.py
+++
@@ -1,9 +0,0 @@
-
-# Вывести строку вида Silence is golden таким образом,
-# чтобы каждое слово было с новой строки.
-
-str = "Silence\nis\ngolden"
-print(str)
-
-# Альтернативный вариант
-# print("Silence\nis\ngolden")
